c4278cdbe2792e9a4ddd0b15167332a7dce909bc
Ray Nguyen <rnguyen7388@gmail.com>
2020-05-26 21:31:51 -0500

    Added local storage

--- script.js
+++ script.js
@@ -3,23 +3,57 @@ var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
-var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+var todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-
-//     * Initially set the text content of the todoList to an empty string.
-function renderTodos() {
-    todoList.innerHTML = "";
-//   * todoCountSpan should show the total count of todos on the page.
-  todoCountSpan.textContent = todos.length;
-// * Inside of your render function you will also need a for loop.
-    for (var i = 0; i < todos.length; i++) {
-//   * It should loop over the `todos` array creating an `li` element for each index of the array.
-    var li = document.createElement("li");
-//   * It should set the content of the created `li` element to the value of the current array index.
-    li.textContent = todos[i];
-//   * Finally the new `li` should be appended to the `ul` provided.
-    todoList.appendChild(li);
-    }
+function updateLocalStorage() {
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
+
+function removeTodo(event) {
+  // it accesses the `data-index` value
+  var todoIndex = parseInt(event.target.parentElement.getAttribute('data-index'));
+  //  and removes that todo element from the list.
+  todos.splice(todoIndex, 1);
+  updateLocalStorage();
+  renderTodos();
+}
+
+function renderTodos() {
+  todoList.innerHTML = "";
+  // * todoCountSpan should show the total cosunt of todos on the page.
+  todoCountSpan.textContent = todos.length;
+  // * Inside of your render function you will also need a for loop.
+  for (var i = 0; i < todos.length; i++) {
+    // * create an `li` element for each index of the array.
+    var li = document.createElement('li');
+    // * It should set the content of the created `li` element to the value of the current array index.
+    li.innerText = todos[i];
+    // * When a new todo is created, add a `data-index` for each `li`.
+    li.setAttribute('data-index', i);
+    //   * Generate a button that says "Complete" and append it to your `li`.
+    var button = document.createElement('button');
+    button.textContent = "Complete";
+    li.appendChild(button);
+    // * Add an event listener so that when a user clicks the Complete button, 
+    button.addEventListener('click', removeTodo);
+    // * Finally the new `li` should be appended to the `ul` provided.'
+    todoList.appendChild(li);
+  }  
+};
+
+todoForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var newTodoText = todoInput.value;
+  // Make sure that empty values are not pushed to the array.
+  if (newTodoText === "") {
+    return;
+  }
+  todos.push(newTodoText);
+  updateLocalStorage();
+  // clear the input field
+  todoInput.value = "";
+  // re-render the todo list.
+  renderTodos();
+});
 
 renderTodos();
